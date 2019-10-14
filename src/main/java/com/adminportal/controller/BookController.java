57e89cf2d3cdc3f4aff442b6f62a1a6416c91958
Marko Miseljic <marko.miseljic.14@gmail.com>
2019-10-14 13:20:27 +0200

    Add new book
    Added controller for book and added html form for add new book

---
+++ src/main/java/com/adminportal/controller/BookController.java
@@ -0,0 +1,18 @@
+package com.adminportal.controller;
+
+import com.adminportal.domain.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+    @RequestMapping("/add")
+    public String addBook(Model model){
+        Book book = new Book();
+        model.addAttribute("book",book);
+        return "addBook";
+    }
+}
