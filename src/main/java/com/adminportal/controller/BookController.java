4d16e6278acf59eaa41e4a125e0a425ad28637cf
Marko Miseljic <marko.miseljic.14@gmail.com>
2019-10-14 15:04:04 +0200

    Backend logick for saving book

--- src/main/java/com/adminportal/controller/BookController.java
+++ src/main/java/com/adminportal/controller/BookController.java
@@ -1,18 +1,50 @@
 package com.adminportal.controller;
 
 import com.adminportal.domain.Book;
+import com.adminportal.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.List;
 
 @Controller
-@RequestMapping("/book")
+@RequestMapping(value = "/book")
 public class BookController {
-    @RequestMapping("/add")
+    @Autowired
+    BookService bookService;
+    @RequestMapping(value = "/add",method = RequestMethod.GET)
     public String addBook(Model model){
         Book book = new Book();
         model.addAttribute("book",book);
         return "addBook";
+    }
+    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    public String addBookPost(@ModelAttribute("book") Book book, HttpServletRequest request){
+        bookService.save(book);
+        MultipartFile bookImage = book.getBookImage();
+        try {
+            byte[] bytes = bookImage.getBytes();
+            String name = book.getId()+"png";
+            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("src/main/resources/static/image/book/"+name)));
+            stream.write(bytes);
+            stream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "redirect:bookList";
+    }
+    @RequestMapping("bookList")
+    public String bookList(Model model){
+//        List<Book> bookList = bookService.findAll();
+        return "bookList";
     }
 }
